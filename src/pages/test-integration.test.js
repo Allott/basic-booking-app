104e7d815799cd0d3e085dc77ca9898984463b7d
Daniel Allott <36487931+Allott@users.noreply.github.com>
2023-10-02 11:35:01 +0100

    TTD - Type and facilities

--- src/pages/test-integration.test.js
+++ src/pages/test-integration.test.js
@@ -6,5 +6,9 @@ describe('Integration Test ', () => {
     it('should render', () => {
         render(<Index />);
         expect(screen.getByText(/Hotel Garni Almhof/)).toBeInTheDocument();
+        expect(screen.getByText(/Alpine bliss!/)).toBeInTheDocument();
+
+        expect(screen.getByText(/Guest House/)).toBeInTheDocument();
+        expect(screen.getByText(/Balcony or Terrace/)).toBeInTheDocument();
     });
 });
